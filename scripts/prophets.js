200e7708d38fdd53de2f2cc329f1ca13f22b341a
bricehardy97 <har24047@byui.edu>
2025-05-12 20:16:34 -0600

    changes

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -3,7 +3,32 @@ const cards = document.querySelector('#cards');
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.prophets); // temporary testing of data response
+    displayProphets(data.prophets)
   }
   
   getProphetData();
+
+  const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+     
+      const card = document.createElement('section');
+      const fullName = document.createElement('h2');
+      const portrait = document.createElement('img');
+  
+    
+      fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+      
+      portrait.setAttribute('src', prophet.imageurl);
+      portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order} Latter-day Prophet`);
+      portrait.setAttribute('loading', 'lazy');
+      portrait.setAttribute('width', '340');
+      portrait.setAttribute('height', '440');
+  
+     
+      card.appendChild(fullName);
+      card.appendChild(portrait);
+  
+
+      cards.appendChild(card);
+    });
+  };
